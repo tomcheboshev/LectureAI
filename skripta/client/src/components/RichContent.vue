c965dbc40abe6ec1a3b52b359707fdbe6eb3eb64
Tomche Boshev <bosevtomce@gmail.com>
2026-07-20 04:45:34 +0200

    style: frame Mermaid diagrams in a bordered card for visual consistency

--- skripta/client/src/components/RichContent.vue
+++ skripta/client/src/components/RichContent.vue
@@ -1,6 +1,8 @@
 <template>
   <template v-for="(seg, i) in segments" :key="i">
-    <MermaidDiagram v-if="seg.type === 'mermaid'" :code="seg.content" />
+    <div v-if="seg.type === 'mermaid'" class="rounded-2xl border border-slate-200 dark:border-border-dark bg-white dark:bg-surface-dark p-4 my-3 overflow-x-auto">
+      <MermaidDiagram :code="seg.content" />
+    </div>
     <div v-else class="rich-content-block" v-html="renderMarkdown(seg.content)"></div>
   </template>
 </template>
